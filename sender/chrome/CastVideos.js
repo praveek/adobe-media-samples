6149df5b06292d73ec86073b7476ddc845129e43
praveeku <praveeku@adobe.com>
2017-07-20 03:01:59 -0700

    Update receiver app id

--- sender/chrome/CastVideos.js
+++ sender/chrome/CastVideos.js
@@ -104,7 +104,7 @@ CastPlayer.prototype.initializeCastPlayer = function() {
     // Set the receiver application ID to your own (created in the
     // Google Cast Developer Console), or optionally
     // use the chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
-    options.receiverApplicationId = 'C85901A0';
+    options.receiverApplicationId = '043E5C0E';
 
     // Auto join policy can be one of the following three:
     // ORIGIN_SCOPED - Auto connect from same appId and page origin
